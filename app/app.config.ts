3ef10593a0696198216714f6bb52aea63f168ae0
Mahito <monester312@gmail.com>
2025-12-03 22:19:39 +0300

    Added CSS and Componenets

---
+++ app/app.config.ts
@@ -0,0 +1,36 @@
+export default defineAppConfig({
+  socials: {
+    //x: "https://x.com/nuxt_js",
+    discord: "https://discord.gg/RZP3A9dq7S",
+    bluesky: "https://bsky.app/profile/mahitron.bsky.social",
+  },
+  github: {
+    rootDir: "docs",
+  },
+  toc: {
+    bottom: {
+      links: [
+        {
+          icon: "i-lucide-book-open",
+          label: "Documentation",
+          to: "https://ui.nuxt.com/getting-started/installation/nuxt",
+          target: "_blank",
+        },
+      ],
+    },
+  },
+  ui: {
+    pageHero: {
+      slots: {
+        title: "font-semibold sm:text-6xl",
+        container: "!pb-0",
+      },
+    },
+    pageCard: {
+      slots: {
+        container: "lg:flex min-w-0",
+        wrapper: "flex-none",
+      },
+    },
+  },
+});
---
+++ app/components/content/DocsAsideRightBottom.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const appConfig = useAppConfig()
+const { t } = useDocusI18n()
+
+// Override the bottom links to include your Discord server
+appConfig.toc = appConfig.toc || {}
+appConfig.toc.bottom = {
+  title: 'Community', // Change the section title
+  links: [
+    {
+      text: 'Join our Discord',
+      link: 'https://discord.gg/YOUR_INVITE_CODE', // Replace with your server invite
+      external: true, // optional, marks it as external
+    },
+  ],
+}
+</script>
+
+<template>
+  <div
+    v-if="appConfig.toc?.bottom?.links?.length"
+    class="hidden lg:block space-y-6"
+  >
+    <USeparator type="dashed" />
+
+    <UPageLinks
+      :title="appConfig.toc?.bottom?.title || t('docs.links')"
+      :links="appConfig.toc?.bottom?.links"
+    />
+  </div>
+</template>
